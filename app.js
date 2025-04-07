ce89f199caab1fcefc53b4c541c9fa388d1da225
Mcorduk <om.corduk@gmail.com>
2025-04-07 17:42:37 +0300

    feat: create the server and listen

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mainRouter from "./routes/index.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Hello, world!");
+});
+
+app.use(mainRouter);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
