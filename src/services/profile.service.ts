b286ac125743ab7093ae541feb5e49647a38bd64
Mcorduk <om.corduk@gmail.com>
2025-04-10 18:30:54 +0300

    refactor: change user table to profile

---
+++ src/services/profile.service.ts
@@ -0,0 +1,15 @@
+import { User } from "@supabase/supabase-js";
+import { Profile } from "../types/db/database.types";
+import supabase from "./supabaseClient";
+
+export async function getAllMessages(): Promise<Profile[] | null> {
+  const { data, error } = await supabase.from("profiles").select("*");
+
+  if (error) {
+    console.error("Error fetching all message:", error);
+
+    return null;
+  }
+
+  return data;
+}
