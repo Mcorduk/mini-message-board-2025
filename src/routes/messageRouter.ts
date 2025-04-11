125776b6692473068c9828e07f73f878d9686584
Mcorduk <om.corduk@gmail.com>
2025-04-11 19:24:03 +0300

    feat: update message router

--- src/routes/messageRouter.ts
+++ src/routes/messageRouter.ts
@@ -12,4 +12,22 @@ messageRouter.get("/:messageId", (req: Request, res: Response) => {
   res.send(`Message Id: ${messageId}`);
 });
 
+messageRouter.post("/", (req: Request, res: Response) => {
+  const { content, chatId } = req.body;
+  res.send("Message Created");
+});
+
+messageRouter.put("/:messageId", (req: Request, res: Response) => {
+  const { messageId } = req.params;
+  const { content } = req.body;
+
+  res.send(`Message Id: ${messageId} updated`);
+});
+
+messageRouter.delete("/:messageId", (req: Request, res: Response) => {
+  const { messageId } = req.params;
+
+  res.send(`Message Id: ${messageId} deleted`);
+});
+
 export default messageRouter;
