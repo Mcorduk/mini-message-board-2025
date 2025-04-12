9870397746ea283fc44617cdd8c7d5adfdc12fe0
Mcorduk <om.corduk@gmail.com>
2025-04-12 18:54:34 +0300

    feat: update member service to have more functions

--- src/services/member.service.ts
+++ src/services/member.service.ts
@@ -18,6 +18,20 @@ export async function getAllMembers(
   return data;
 }
 
+export async function getProfileMemberships(profileId: string) {
+  const { data, error } = await supabase
+    .from("members")
+    .select("*")
+    .eq("profile_id", profileId);
+
+  if (error) {
+    console.error("Error fetching members for chatroom:", error);
+    return null;
+  }
+
+  return data;
+}
+
 export async function createMember(
   newMember: Partial<Member>
 ): Promise<Member | null> {
@@ -33,4 +47,33 @@ export async function createMember(
   }
 
   return data;
+}
+
+export async function updateMember(
+  memberId: number,
+  updates: Partial<Member>
+): Promise<Member | null> {
+  const { data, error } = await supabase
+    .from("members")
+    .update(updates)
+    .eq("id", memberId)
+    .single();
+
+  if (error) {
+    console.error("Error updating Member:", error);
+    return null;
+  }
+
+  return data;
+}
+
+export async function deleteMember(memberId: number): Promise<boolean> {
+  const { error } = await supabase.from("members").delete().eq("id", memberId);
+
+  if (error) {
+    console.error("Error deleting Member:", error);
+    return false;
+  }
+
+  return true;
 }
