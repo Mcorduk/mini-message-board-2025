4b9ff0055c8912eadfe8f811862726e25c0a8af3
Mcorduk <om.corduk@gmail.com>
2025-04-12 18:54:40 +0300

    feat: update profile service to have more functions

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,7 +1,7 @@
 import { Profile } from "../types/db/database.types";
 import supabase from "./supabaseClient";
 
-export async function getAllMessages(): Promise<Profile[] | null> {
+export async function getAllProfiles(): Promise<Profile[] | null> {
   const { data, error } = await supabase.from("profiles").select("*");
 
   if (error) {
@@ -12,3 +12,49 @@ export async function getAllMessages(): Promise<Profile[] | null> {
 
   return data;
 }
+
+export async function getProfile(profileId: string): Promise<Profile | null> {
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("profile_id", profileId)
+    .single();
+
+  if (error) {
+    console.error("Error fetching all message:", error);
+
+    return null;
+  }
+
+  return data;
+}
+
+export async function updateProfile(
+  profileId: string,
+  updates: Partial<Profile>
+): Promise<Profile | null> {
+  const { data, error } = await supabase
+    .from("profiles")
+    .update(updates)
+    .eq("id", profileId)
+    .single();
+
+  if (error) {
+    console.error("Error updating Profile:", error);
+    return null;
+  }
+
+  return data;
+}
+
+// Currently risky to use
+// export async function deleteProfile(profileId: number): Promise<boolean> {
+//   const { error } = await supabase.from("profiles").delete().eq("id", profileId);
+
+//   if (error) {
+//     console.error("Error deleting Profile:", error);
+//     return false;
+//   }
+
+//   return true;
+// }
