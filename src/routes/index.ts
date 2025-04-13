9921281595a8ff71a5cbd57a79b0df89b40058ab
Mcorduk <om.corduk@gmail.com>
2025-04-13 17:01:37 +0300

    feat: update the new router main endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import messageRouter from "./messageRouter.js";
+import messageRouter from "./message.router.js";
+import memberRouter from "./member.router.js";
+import profileRouter from "./profile.router.js";
+import chatroomRouter from "./chatroom.router.js";
 
 const mainRouter = Router();
 
 mainRouter.use("/messages", messageRouter);
+mainRouter.use("/profiles", profileRouter);
+mainRouter.use("chatrooms", chatroomRouter);
+mainRouter.use("/members", memberRouter);
 
 export default mainRouter;
