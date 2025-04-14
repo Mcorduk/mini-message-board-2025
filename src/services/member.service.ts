28535d78ca79cc852d8d357a033dcc806b4d6c20
Mcorduk <om.corduk@gmail.com>
2025-04-14 19:14:15 +0300

    refactor: turn the service functions into a class

--- src/services/member.service.ts
+++ src/services/member.service.ts
@@ -1,78 +1,79 @@
 import { Member } from "../types/db/database.types";
 import supabase from "./supabaseClient";
 
-export async function getAllMembers(
-  chatroomId: number
-): Promise<Member[] | null> {
-  const { data, error } = await supabase
-    .from("members")
-    .select("*")
-    .eq("id", chatroomId);
+export class MessageService {
+  async getAllMembers(chatroomId: number): Promise<Member[] | null> {
+    const { data, error } = await supabase
+      .from("members")
+      .select("*")
+      .eq("id", chatroomId);
 
-  if (error) {
-    console.error("Error fetching members for chatroom:", error);
-    return null;
+    if (error) {
+      console.error("Error fetching members for chatroom:", error);
+      return null;
+    }
+
+    return data;
   }
 
-  return data;
-}
+  async getProfileMemberships(profileId: string) {
+    const { data, error } = await supabase
+      .from("members")
+      .select("*")
+      .eq("profile_id", profileId);
 
-export async function getProfileMemberships(profileId: string) {
-  const { data, error } = await supabase
-    .from("members")
-    .select("*")
-    .eq("profile_id", profileId);
+    if (error) {
+      console.error("Error fetching members for chatroom:", error);
+      return null;
+    }
 
-  if (error) {
-    console.error("Error fetching members for chatroom:", error);
-    return null;
+    return data;
   }
 
-  return data;
-}
+  async createMember(newMember: Partial<Member>): Promise<Member | null> {
+    const { data, error } = await supabase
+      .from("members")
+      .insert(newMember)
+      .single();
 
-export async function createMember(
-  newMember: Partial<Member>
-): Promise<Member | null> {
-  const { data, error } = await supabase
-    .from("members")
-    .insert(newMember)
-    .single();
+    if (error) {
+      console.error("Error creating Member:", error);
 
-  if (error) {
-    console.error("Error creating Member:", error);
+      return null;
+    }
 
-    return null;
+    return data;
   }
 
-  return data;
-}
+  async updateMember(
+    memberId: number,
+    updates: Partial<Member>
+  ): Promise<Member | null> {
+    const { data, error } = await supabase
+      .from("members")
+      .update(updates)
+      .eq("id", memberId)
+      .single();
 
-export async function updateMember(
-  memberId: number,
-  updates: Partial<Member>
-): Promise<Member | null> {
-  const { data, error } = await supabase
-    .from("members")
-    .update(updates)
-    .eq("id", memberId)
-    .single();
+    if (error) {
+      console.error("Error updating Member:", error);
+      return null;
+    }
 
-  if (error) {
-    console.error("Error updating Member:", error);
-    return null;
+    return data;
   }
 
-  return data;
-}
+  async deleteMember(memberId: number): Promise<boolean> {
+    const { error } = await supabase
+      .from("members")
+      .delete()
+      .eq("id", memberId);
 
-export async function deleteMember(memberId: number): Promise<boolean> {
-  const { error } = await supabase.from("members").delete().eq("id", memberId);
+    if (error) {
+      console.error("Error deleting Member:", error);
+      return false;
+    }
 
-  if (error) {
-    console.error("Error deleting Member:", error);
-    return false;
+    return true;
   }
-
-  return true;
 }
