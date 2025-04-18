803faf39c98d3ed5333ec4947de3b4b2f027a453
Mcorduk <om.corduk@gmail.com>
2025-04-18 19:02:23 +0300

    feat: add more methods to message controller

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { MessageService } from "../services/message.service";
+import { Message } from "../types/db/database.types";
 
 export class MessageController {
   private messageService: MessageService;
@@ -10,10 +11,8 @@ export class MessageController {
 
   async getAllMessages(req: Request, res: Response) {
     try {
-      const { chatroomId } = req.query;
-      const messages = await this.messageService.getAllMessages(
-        parseInt(chatroomId)
-      );
+      const chatroomId = Number(req.query.chatroomId);
+      const messages = await this.messageService.getAllMessages(chatroomId);
 
       if (!messages) {
         return res.status(404).json({ error: "Messages not found" });
@@ -24,4 +23,64 @@ export class MessageController {
       res.status(500).json({ error: "Failed to fetch messages" });
     }
   }
+
+  async createMessage = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const newMessage: Partial<Message> = req.body;
+      const createdMessage = await this.messageService.createMessage(newMessage);
+
+      if (!createdMessage) {
+        res.status(400).json({ error: "Failed to create message" });
+        return;
+      }
+
+      res.status(201).json(createdMessage);
+    } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+
+  async updateMessage = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const messageId = Number(req.params.messageId);
+
+      if (isNaN(messageId)) {
+        res.status(400).json({ error: "Invalid message ID" });
+        return;
+      }
+      const updates: Partial<Message> = req.body;
+      const updatedMessage = await this.messageService.updateMessage(messageId, updates);
+
+      if (!updatedMessage) {
+        res.status(404).json({ error: "Message not found" });
+        return;
+      }
+
+      res.status(200).json(updatedMessage);
+    } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
+
+
+  async deleteMessage = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const messageId = Number(req.params.messageId);
+      if (isNaN(messageId)) {
+        res.status(400).json({ error: "Invalid message ID" });
+        return;
+      }
+
+      const deletedMessage = await this.messageService.deleteMessage(messageId);
+      if (!deletedMessage) {
+        res.status(404).json({ error: "Message not found" });
+        return;
+      }
+
+      res.status(200).json({ message: "Message deleted successfully" });
+    } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
 }
