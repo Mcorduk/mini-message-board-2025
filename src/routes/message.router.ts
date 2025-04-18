6e109c61ee94f8a061a596ce8476d9f9db6665f3
Mcorduk <om.corduk@gmail.com>
2025-04-18 19:02:35 +0300

    fix: message router

--- src/routes/message.router.ts
+++ src/routes/message.router.ts
@@ -1,32 +1,23 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import { MessageController } from "../controllers/message.controller";
 
-const messageRouter = Router();
+const messageRouter: Router = Router();
+const messageController: MessageController = new MessageController();
 
-messageRouter.get("/", (req: Request, res: Response) => {
-  res.send("All messages");
-});
+messageRouter.get("/", (req, res) =>
+  messageController.getAllMessages(req, res)
+);
 
-messageRouter.get("/:messageId", (req: Request, res: Response) => {
-  const { messageId } = req.params;
-  res.send(`Message Id: ${messageId}`);
-});
+messageRouter.post("/", (req, res) =>
+  messageController.createMessage(req, res)
+);
 
-messageRouter.post("/", (req: Request, res: Response) => {
-  const { content, chatId } = req.body;
-  res.send("Message Created");
-});
+messageRouter.put("/:messageId", (req, res) =>
+  messageController.updateMessage(req, res)
+);
 
-messageRouter.put("/:messageId", (req: Request, res: Response) => {
-  const { messageId } = req.params;
-  const { content } = req.body;
-
-  res.send(`Message Id: ${messageId} updated`);
-});
-
-messageRouter.delete("/:messageId", (req: Request, res: Response) => {
-  const { messageId } = req.params;
-
-  res.send(`Message Id: ${messageId} deleted`);
-});
+messageRouter.delete("/:messageId", (req, res) =>
+  messageController.deleteMessage(req, res)
+);
 
 export default messageRouter;
