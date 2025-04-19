9d76911e8631599225e5d7f395d468d9f5a0e55d
Mcorduk <om.corduk@gmail.com>
2025-04-19 17:14:36 +0300

    fix: properly handle async methods

--- src/routes/message.router.ts
+++ src/routes/message.router.ts
@@ -4,20 +4,20 @@ import { MessageController } from "../controllers/message.controller";
 const messageRouter: Router = Router();
 const messageController: MessageController = new MessageController();
 
-messageRouter.get("/", (req, res) =>
-  messageController.getAllMessages(req, res)
-);
+messageRouter.get("/", (req, res) => {
+  messageController.getAllMessages(req, res);
+});
 
-messageRouter.post("/", (req, res) =>
-  messageController.createMessage(req, res)
-);
+messageRouter.post("/", (req, res) => {
+  messageController.createMessage(req, res);
+});
 
-messageRouter.put("/:messageId", (req, res) =>
-  messageController.updateMessage(req, res)
-);
+messageRouter.put("/:messageId", (req, res) => {
+  messageController.updateMessage(req, res);
+});
 
-messageRouter.delete("/:messageId", (req, res) =>
-  messageController.deleteMessage(req, res)
-);
+messageRouter.delete("/:messageId", (req, res) => {
+  messageController.deleteMessage(req, res);
+});
 
 export default messageRouter;
